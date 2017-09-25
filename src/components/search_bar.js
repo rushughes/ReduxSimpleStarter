9829fa63ff36fce55a4531b8a2cd1bae924b2834
Rus Hughes <russell.hughes@gmail.com>
2017-09-25 16:00:44 +0700

    search bar now logs to console

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Component} from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    return <input />;
+    return <input onChange={this.onInputChange} />;
   }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
 };
 
 export default SearchBar;
