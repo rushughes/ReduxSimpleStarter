490c0e06cbf022a8bcce28e4d3468ee5d9e005f1
Rus Hughes <russell.hughes@gmail.com>
2017-09-25 16:14:24 +0700

    we now display the input on the screen by setting state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,19 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
-    return <input onChange={(event) => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input onChange={(event) => this.setState({ term:  event.target.value })} />;
+        value of the input: {this.state.term}
+      </div>
+    );
   }
 
 };
